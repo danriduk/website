bd81dd91dbbbb50ce415d6b0d5793a84b2e62e90
danriduk <danriduk@gmail.com>
2018-07-04 22:29:58 +1300

    Update sketchTest.js

--- _sketches/sketchTest.js
+++ _sketches/sketchTest.js
@@ -2,9 +2,6 @@
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-  canvas.style('display', 'block');
   canvas.parent('sketchTest-holder');
-
-  canvas.position(windowWidth/2, windowHeight/2)
   background(50, 50, 50);
 }
